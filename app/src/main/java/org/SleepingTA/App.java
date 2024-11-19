fb94b3674fcb06d99ff56ce6eb11c32fbfb4a554
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-19 19:15:54 +0200

    Rebuild the project for optimization

--- app/src/main/java/org/SleepingTA/App.java
+++ app/src/main/java/org/SleepingTA/App.java
@@ -5,32 +5,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Number of students: ");
-        int numStudents = input.nextInt();
+
+        int numChairs, numTAs, numStudents;
+        System.out.print("Number of Students: ");
+        numStudents = input.nextInt();
         System.out.print("Number of chairs: ");
-        int numChairs = input.nextInt();
-        System.out.print("Number of Teaching assistants: ");
-        int numTAs = input.nextInt();
+        numChairs = input.nextInt();
+        System.out.print("Number of TAs: ");
+        numTAs = input.nextInt();
         input.close();
-
-        WaitingArea waitingArea = new WaitingArea(numChairs);
-
-        // Start the TA thread
-        for (int i = 1; i <= numTAs; i++) {
-            TA ta = new TA(waitingArea);
-            ta.start();
-        }
-
-        // Start student threads
-        for (int i = 1; i <= numStudents; i++) {
-            Student student = new Student(i, waitingArea);
-            student.start();
-
-            try {
-                Thread.sleep(500); // Stagger student arrivals
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
