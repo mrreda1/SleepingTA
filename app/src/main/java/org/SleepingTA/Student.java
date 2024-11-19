a89eb8ba2b6e5e001b20eca30befbff4548a4c75
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-19 20:12:20 +0200

    Student class init

---
+++ app/src/main/java/org/SleepingTA/Student.java
@@ -0,0 +1,18 @@
+package org.SleepingTA;
+
+/**
+ * Student
+ */
+public class Student extends Thread {
+    private int id;
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+        }
+    }
+}
