ea680ae5c96506feb271d4e6f6bab926583ee0f3
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-19 20:36:15 +0200

    Student class completed

--- app/src/main/java/org/SleepingTA/Student.java
+++ app/src/main/java/org/SleepingTA/Student.java
@@ -1,9 +1,12 @@
 package org.SleepingTA;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Student
  */
 public class Student extends Thread {
+    @SuppressWarnings("unused")
     private int id;
 
     public Student(int id) {
@@ -13,6 +16,24 @@ public class Student extends Thread {
     @Override
     public void run() {
         while (true) {
+            if (App.getChairs().tryAcquire()) {
+                try {
+                    App.getTAs().acquire();
+                    App.getChairs().release();
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(5, 8) * 1000);
+                    App.getTAs().release();
+                    return;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Try again after random period of time
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(3, 8) * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
